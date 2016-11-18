7b1413a45f85b668a4fed664070371dafe051901
Valentin Alexeev <valentin.alekseev@gmail.com>
2016-11-18 14:24:53 +0200

    Supply user-agent string for a proper request.

--- pwaqi/__init__.py
+++ pwaqi/__init__.py
@@ -1,15 +1,49 @@
 import requests
+from urllib.parse import unquote
+from datetime import datetime
 
 def findStationCodesByCity(cityName):
 	r = requests.get("https://wind.waqi.info/nsearch/station/" + cityName)
 
 	if r.status_code == 200:
-		return [result["x"] for result in r.json()["results"]]
+		return r.json()["results"]
 	else:
 		return []
 
-def getStationObservation(stationCode, language = "en"):
-	r = requests.get("https://waqi.info/api/feed/@" + str(stationCode) + "/obs." + language + ".json")
+def decoder(todecode):
+	decoded = [chr(ord(c)-1) for c in unquote(todecode)]
+	return "".join(decoded)
+
+def getJSKey(locationName):
+	r = requests.get("http://aqicn.org/city/" + locationName)
+	if r.status_code == 200:
+		pos = r.text.find("decodeURIComponent")
+		todecode = r.text[pos+20:pos+76]
+		key = decoder(todecode)
+		print("key = {}".format(key))
+		return key 
+	return ""
+
+def getToken(stationCode):
+	rToken = requests.get("https://waqi.info/api/token/" + str(stationCode))
+	print("token = {}".format(unquote(rToken.json()["rxs"]["obs"][0]["msg"]["token"])))
+	return unquote(rToken.json()["rxs"]["obs"][0]["msg"]["token"])
+
+def getUID():
+	return  "abcde" + datetime.now().strftime("%s000")
+
+def getStationObservation(stationCode, locationName='', language = "en"):
+	r = requests.post("https://waqi.info/api/feed/@" + str(stationCode) + "/obs." + language + ".json",
+		data = {
+#			'token': getToken(stationCode),
+#			'key': getJSKey(locationName),
+#			'uid': getUID(),
+#			'rqc': '2'
+			},
+		headers = {
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36 OPR/41.0.2353.56',
+#			'referer': 'http://aqicn.org/city' + locationName
+			})
 
 	if r.status_code == 200:
 		obsJson = r.json()["rxs"]["obs"][0]["msg"]
@@ -21,7 +55,8 @@ def getStationObservation(stationCode, language = "en"):
 			'city': obsJson.get('city', ''),
 			'aqi': obsJson['aqi'],
 			'iaqi': [iaqiTranslate(item) for item in obsJson['iaqi']],
-			'dominentpol': obsJson.get("dominentpol", '')
+			'dominentpol': obsJson.get("dominentpol", ''),
+                        'time': obsJson['time']['v']
 			}
 	else:
 		return {}
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pwaqi',
-	version='1.0',
+	version='1.1',
 	description='Python wrapper for World Air Quality Index data',
 	author='Valentin Alexeev',
 	author_email='valentin.alekseev@gmail.com',
