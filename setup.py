8825c9cb7ecf9288144ad93fe71cb962a34875ba
Valentin Alexeev <valentin.alekseev@gmail.com>
2017-02-02 11:42:53 +0200

    Bump version to 1.4
    Fix a typo - merged PR from @bepcyc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pwaqi',
-	version='1.3',
+	version='1.4',
 	description='Python wrapper for World Air Quality Index data',
 	author='Valentin Alexeev',
 	author_email='valentin.alekseev@gmail.com',
