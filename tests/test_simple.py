3276b846efb91d9a6d5f9321825aa371bccd25ee
Valentin Alexeev <valentin.alekseev@gmail.com>
2017-03-03 16:26:51 +0200

    Real API token
    Add ability to provide real API token to test coverage of more code.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,17 +1,29 @@
+import os
+
 def test_search():
     import pwaqi
 
-    res = pwaqi.findStationCodesByCity('bangalore', 'demo')
+    token = os.environ.get('AQICN_API_TOKEN', 'demo')
+    res = pwaqi.findStationCodesByCity('bangalore', token)
 
     assert len(res) > 0
     assert 8190 in res
+
+def test_invalid_location():
+    import pwaqi
+
+    token = os.environ.get('AQICN_API_TOKEN', 'demo')
+    res = pwaqi.findStationCodesByCity('INVALID_CITY_NAME', token)
+
+    assert len(res) == 0
 
 
 def test_observations():
     import pwaqi
 
     station = 1437
-    res = pwaqi.get_station_observation(station, 'demo')
+    token = os.environ.get('AQICN_API_TOKEN', 'demo')
+    res = pwaqi.get_station_observation(station, token)
 
     assert 'city' in res
     assert station == res['idx']
@@ -20,7 +32,8 @@ def test_observations():
 def test_no_station():
     import pwaqi
 
-    station = 1
-    res = pwaqi.get_station_observation(station, 'demo')
+    station = 10000
+    token = os.environ.get('AQICN_API_TOKEN', 'demo')
+    res = pwaqi.get_station_observation(station, token)
 
-    assert res['idx'] == 1437
+    assert res['idx'] == -1
